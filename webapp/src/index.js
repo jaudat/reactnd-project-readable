c99a188b09c33ad7c8c2419869cefcfd91271501
Jaudat Syed <sjh.jaudat@gmail.com>
2017-09-10 14:56:22 -0400

    using compose properly

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -4,8 +4,7 @@ import './index.css';
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 
-// import {createStore, applyMiddleware, combineReducers, compose} from 'redux'
-import {createStore, applyMiddleware, combineReducers} from 'redux'
+import {createStore, applyMiddleware, combineReducers, compose} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
 import {BrowserRouter} from 'react-router-dom'
@@ -32,9 +31,8 @@ const rootReducer = combineReducers({
   pageNotFound: pageNotFoundReducer
 })
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ || compose
-// const store = createStore( rootReducer, composeEnhancers( applyMiddleware(thunk) )  )
-const store = createStore( rootReducer, applyMiddleware(thunk)  )
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore( rootReducer, {}, composeEnhancers( applyMiddleware(thunk) ) )
 
 ReactDOM.render(
   <Provider store={store}>
